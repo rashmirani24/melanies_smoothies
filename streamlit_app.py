5fd76d301545cae06e109a47c19034f57887095b
Rashmi Rani <63360060+rashmirani24@users.noreply.github.com>
2025-10-08 01:40:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ connection_parameters = {
 session = Session.builder.configs(connection_parameters).create()
 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col(SEARCH_ON))
 
 
 ingredients_list = st.multiselect('Choose upto 5 ingredients: ',my_dataframe,max_selections=5)
